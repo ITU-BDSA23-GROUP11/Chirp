e389c140ba9a0847fc4dd84ee6b2499138f470d5
Marcus Aandahl <maraa@itu.dk>
2023-09-20 20:13:12 +0200

    Removed NUnit in favor of XUnit

--- test/Chirp.CSVDB.Tests/UnitTests.cs
+++ test/Chirp.CSVDB.Tests/UnitTests.cs
@@ -1,15 +1,11 @@
 namespace Chirp.CSVDB.Tests;
 
-public class Tests
+public class UnitTests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
 
-    [Test]
+    [Fact]
     public void Test1()
     {
-        Assert.Pass();
+        Assert.True(true);
     }
 }
