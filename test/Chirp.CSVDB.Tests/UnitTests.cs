0c3397d84d2e2a23abf4f30b5ac06b722d4e4cd1
Marcus Aandahl <marcus.aandahl@gmail.com>
2023-09-21 16:24:13 +0200

    Re-added time conversion test

--- test/Chirp.CSVDB.Tests/UnitTests.cs
+++ test/Chirp.CSVDB.Tests/UnitTests.cs
@@ -2,10 +2,16 @@ namespace Chirp.CSVDB.Tests;
 
 public class UnitTests
 {
-
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("18/09/23 14:38:59", 1695040739, true)]
+    [InlineData("NaN", -100000000, true)]
+    [InlineData("18/09/23 14:38:59", 1692440719, false)]
+    [InlineData("NaN", 1, false)]
+    public void TextUnixConversionEqual(String timestamp, long unix, bool expectedToMatch)
     {
-        Assert.True(true);
+        String converted = Conversion.TimeStampConversion(unix);
+        bool match = timestamp.Equals(converted);
+
+        Assert.Equal(match, expectedToMatch);
     }
 }
