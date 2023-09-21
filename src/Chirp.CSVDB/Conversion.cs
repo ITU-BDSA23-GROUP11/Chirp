5b77acac16082975cac1995755fa242908bebf01
Marcus Aandahl <marcus.aandahl@gmail.com>
2023-09-21 15:51:39 +0200

    Minor refactor + changed to xunit

---
+++ src/Chirp.CSVDB/Conversion.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Chirp.CSVDB;
+
+public class Conversion
+{
+    public static string TimeStampConversion(long unix) {
+        DateTimeOffset dto = DateTimeOffset.FromUnixTimeSeconds(unix).AddHours(2);//Account for time difference
+            
+        CultureInfo customCulture = new CultureInfo(CultureInfo.CurrentCulture.Name);
+        customCulture.DateTimeFormat.DateSeparator = "/";
+        customCulture.DateTimeFormat.TimeSeparator = ":";
+
+        String date = dto.ToString("dd/MM/yy HH:mm:ss", customCulture);
+            
+        string regexPattern = @"^(0[1-9]|[1-2][0-9]|3[01])/(0[1-9]|1[0-2])/(\d{2}) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$";
+            
+        //Verify that the date is in the correct format
+        bool matches = Regex.IsMatch(date, regexPattern);
+
+        return (matches && dto.Year >= 1970) ? date : "NaN";
+    }
+}
--- src/Chirp.CSVDB/CsvDatabase.cs
+++ src/Chirp.CSVDB/CsvDatabase.cs
@@ -2,8 +2,6 @@ using CsvHelper;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
-
-
 namespace Chirp.CSVDB
 
 {
@@ -20,7 +18,7 @@ namespace Chirp.CSVDB
 
         public void AddCheep(Cheep cheep)
         {
-            using (var sw = new StreamWriter(_filePath, append: true))
+            using (var sw = new StreamWriter(@_filePath, append: true))
             using (var csv = new CsvWriter(sw, CultureInfo.InvariantCulture))
             {
                 csv.WriteRecord(cheep);
@@ -38,28 +36,11 @@ namespace Chirp.CSVDB
                 while (csv.Read())
                 {
                     var cheep = csv.GetRecord<Cheep>();
-                    cheepsList.Add($"{cheep.Author} @ {TimeStampConversion(cheep.Timestamp)}: {cheep.Message}");
+                    cheepsList.Add($"{cheep.Author} @ {Conversion.TimeStampConversion(cheep.Timestamp)}: {cheep.Message}");
                 }
             }
 
             return cheepsList;
-        }
-        public static string TimeStampConversion(long unix)
-        {
-            DateTimeOffset dto = DateTimeOffset.FromUnixTimeSeconds(unix).AddHours(2);//Account for time difference
-            
-            CultureInfo customCulture = new CultureInfo(CultureInfo.CurrentCulture.Name);
-            customCulture.DateTimeFormat.DateSeparator = "/";
-            customCulture.DateTimeFormat.TimeSeparator = ":";
-
-            String date = dto.ToString("dd/MM/yy HH:mm:ss", customCulture);
-            
-            string regexPattern = @"^(0[1-9]|[1-2][0-9]|3[01])/(0[1-9]|1[0-2])/(\d{2}) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$";
-            
-            //Verify that the date is in the correct format
-            bool matches = Regex.IsMatch(date, regexPattern);
-            if (matches && dto.Year >= 1970) return date;
-            else return "NaN";
         }
     }
 }
