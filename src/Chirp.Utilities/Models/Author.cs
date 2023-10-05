ad7521d0a0d90263bc485f825bc0615e8731e5da
Marcus Aandahl <marcus.aandahl@gmail.com>
2023-10-05 13:14:18 +0200

    Updated Models to enable 1 to many ORM relationship

--- src/Chirp.Utilities/Models/Author.cs
+++ src/Chirp.Utilities/Models/Author.cs
@@ -4,15 +4,7 @@ public class Author
 {
     public string Id { get; } = Guid.NewGuid().ToString();
     public required string Name { get; set; }
-    public ICollection<Cheep> Cheeps { get; set; } = new HashSet<Cheep>(); // Consider if necessary
-
-    public Author(string name)
-    {
-        Name = name;
-    }
     
-    public void AddCheep(Cheep cheep)
-    {
-        Cheeps.Add(cheep);
-    }
+    // 1 Author to Many Cheeps relation
+    public ICollection<Cheep> Cheeps { get; set; } = new List<Cheep>();
 }
--- src/Chirp.Utilities/Models/Cheep.cs
+++ src/Chirp.Utilities/Models/Cheep.cs
@@ -3,12 +3,11 @@ namespace Chirp.Utilities.Models;
 public class Cheep
 {
     public string Id { get; } = Guid.NewGuid().ToString();
+    
+    // 1 Cheep to 1 Author relation
+    public string AuthorId { get; set; }
     public required Author Author { get; set; }
+    
     public string Message { get; set; } = "";
     public DateTime Timestamp { get; } = DateTime.UtcNow;
-
-    public Cheep(Author author)
-    {
-        Author = author;
-    }
 }
