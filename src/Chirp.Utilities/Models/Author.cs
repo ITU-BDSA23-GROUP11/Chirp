3117c8f18d5fa0270dcedbd64f20065826541c2a
Marcus Aandahl <marcus.aandahl@gmail.com>
2023-10-05 12:53:59 +0200

    Made Model fields public + added add cheep to Author method

--- src/Chirp.Utilities/Models/Author.cs
+++ src/Chirp.Utilities/Models/Author.cs
@@ -2,7 +2,12 @@ namespace Chirp.Utilities.Models;
 
 public class Author
 {
-    private string Id { get; } = Guid.NewGuid().ToString();
-    public string Name { get; set; } = "";
+    public string Id { get; } = Guid.NewGuid().ToString();
+    public required string Name { get; set; }
     public ICollection<Cheep> Cheeps { get; set; } = new HashSet<Cheep>(); // Consider if necessary
+
+    public void AddCheep(Cheep cheep)
+    {
+        Cheeps.Add(cheep);
+    }
 }
--- src/Chirp.Utilities/Models/Cheep.cs
+++ src/Chirp.Utilities/Models/Cheep.cs
@@ -2,8 +2,8 @@ namespace Chirp.Utilities.Models;
 
 public class Cheep
 {
-    private string Id { get; } = Guid.NewGuid().ToString();
+    public string Id { get; } = Guid.NewGuid().ToString();
     public required Author Author { get; set; }
     public string Message { get; set; } = "";
-    private DateTime Timestamp { get; } = DateTime.UtcNow;
+    public DateTime Timestamp { get; } = DateTime.UtcNow;
 }
