8ae7056c30a968b47505d62c3bb8b9724d897aec
Marcus Aandahl <marcus.aandahl@gmail.com>
2023-10-08 15:08:22 +0200

    Made ChirpDBContext.cs to a singleton Co-authored-by: Villads Grum-Schwensen <vilg@itu.dk> Co-authored-by: Andreas Bartholdy <anbc@itu.dk>

--- src/Chirp.DBService/Repositories/CheepRepository.cs
+++ src/Chirp.DBService/Repositories/CheepRepository.cs
@@ -4,36 +4,35 @@ namespace Chirp.DBService.Repositories;
 
 public class CheepRepository : ICheepRepository
 {
-    private readonly ChirpDBContext _chirpDbContext = new ();
     public CheepRepository()
     {
-        DbInitializer.SeedDatabase(_chirpDbContext);
+        DbInitializer.SeedDatabase(ChirpDBContext.GetInstance());
     }
     
     public Cheep AddCheep(Cheep cheep)
     {
-        _chirpDbContext.Cheeps.Add(cheep);
-        _chirpDbContext.SaveChanges();
+        ChirpDBContext.GetInstance().Cheeps.Add(cheep);
+        ChirpDBContext.GetInstance().SaveChanges();
         return cheep;
     }
 
     public List<Cheep> GetCheeps()
     {
-        return _chirpDbContext.Cheeps.ToList();
+        return ChirpDBContext.GetInstance().Cheeps.ToList();
     }
     
     public List<Cheep> GetCheepsForPage(int pageNumber)
     {
-       return _chirpDbContext.Cheeps.Skip(pageNumber * 32).Take(32).ToList();
+       return ChirpDBContext.GetInstance().Cheeps.Skip(pageNumber * 32).Take(32).ToList();
     }
 
     public int GetCheepCount()
     {
-        return _chirpDbContext.Cheeps.Count();
+        return ChirpDBContext.GetInstance().Cheeps.Count();
     }
 
     public List<Cheep> GetCheepsFromAuthorName(string authorName)
     {
-        return _chirpDbContext.Cheeps.Where(c => c.Author.Name == authorName).ToList();
+        return ChirpDBContext.GetInstance().Cheeps.Where(c => c.Author.Name == authorName).ToList();
     }
 }
