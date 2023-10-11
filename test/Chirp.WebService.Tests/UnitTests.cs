8061348db4fc026ac0ee439f3b6a437514766e87
Marcus Aandahl <marcus.aandahl@gmail.com>
2023-10-11 14:10:37 +0200

    Added test to ensure code coverage workflow works

---
+++ test/Chirp.WebService.Tests/UnitTests.cs
@@ -0,0 +1,11 @@
+namespace Chirp.Utilities.Tests;
+
+public class UnitTests
+{
+    [Fact]
+    public void PlaceholderTest()
+    {
+        // Placeholder test to ensure code coverage workflow doesn't fail
+        Assert.True(true);
+    }
+}
