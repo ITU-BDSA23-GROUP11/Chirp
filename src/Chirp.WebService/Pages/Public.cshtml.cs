c4cd13a24625938177e8abafb96b7599fff14337
Villads Grum-Schwensen <vilg@itu.dk>
2023-10-11 14:48:41 +0200

    Implemented safety check to ensure user does not access a page parameter that is too large. If so, return max page.

--- src/Chirp.WebService/Pages/Public.cshtml.cs
+++ src/Chirp.WebService/Pages/Public.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Chirp.DBService.Models;
 using Chirp.DBService.Repositories;
+using Chirp.Utilities;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,6 +10,9 @@ namespace Chirp.WebService.Pages;
 public class PublicModel : PageModel
 {
     private readonly ICheepRepository _service;
+
+    public int pageNumber { get; set; }
+
     public List<Cheep> Cheeps { get; set; }
 
     public int AmountOfPages { get; set; }
@@ -19,10 +24,23 @@ public class PublicModel : PageModel
 
     public ActionResult OnGet()
     {
-        Cheeps = _service.GetCheepsWithAuthors();
+        //Determine pageNumber
+        if (Request.Query.ContainsKey("page") && int.TryParse(Request.Query["page"], out int _pageNumber))
+        {
+            //If parameter is too large -> set to max
+            if (_pageNumber > AmountOfPages) pageNumber = AmountOfPages;
+            else pageNumber = _pageNumber;
+        }
+        else
+        {
+            //Fallback page 0
+            pageNumber = 0;
+        }
         
+        Cheeps = _service.GetCheepsForPage(pageNumber);
+            
         //Calculate the amount of pages needed
-        AmountOfPages = (int)Math.Ceiling((double)Cheeps.Count() / 32);
+        AmountOfPages = (int)Math.Ceiling((double)_service.GetCheepCount() / 32);
         return Page();
     }
 }
--- src/Chirp.WebService/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.WebService/Pages/UserTimeline.cshtml.cs
@@ -8,7 +8,11 @@ namespace Chirp.WebService.Pages;
 public class UserTimelineModel : PageModel
 {
     private readonly ICheepRepository _service;
+    
+    public int pageNumber { get; set; }
     public List<Cheep> Cheeps { get; set; }
+    
+    public int AmountOfPages { get; set; }
 
     public UserTimelineModel(ICheepRepository service)
     {
@@ -17,7 +21,23 @@ public class UserTimelineModel : PageModel
 
     public ActionResult OnGet(string author)
     {
-        Cheeps = _service.GetCheepsFromAuthorNameWithAuthors(author);
+        //Determine pageNumber
+        if (Request.Query.ContainsKey("page") && int.TryParse(Request.Query["page"], out int _pageNumber))
+        {
+            //If parameter is too large -> set to max
+            if (_pageNumber > AmountOfPages) pageNumber = AmountOfPages;
+            else pageNumber = _pageNumber;
+        }
+        else
+        {
+            //Fallback page 0
+            pageNumber = 0;
+        }
+        
+        Cheeps = _service.GetCheepsFromAuthorNameForPage(author, pageNumber);
+            
+        //Calculate the amount of pages needed
+        AmountOfPages = (int)Math.Ceiling((double)_service.GetCheepsFromAuthorNameWithAuthors(author).Count() / 32);
         return Page();
     }
 }
