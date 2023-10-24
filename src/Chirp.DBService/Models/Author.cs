56df91535ed5c814cd0be1261a3f532d922ca229
Andreas <anbc@itu.dk>
2023-10-24 18:44:58 +0200

    Regular expression used to make a correct email format

--- src/Chirp.DBService/Models/Author.cs
+++ src/Chirp.DBService/Models/Author.cs
@@ -8,6 +8,10 @@ public class Author
     [Key]
     public Guid AuthorId { get; set; }
     public required string Name { get; set; }
+    [Required]
+    [MaxLength(50)]
+    [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", 
+        ErrorMessage = "Invalid email format.")] 
     public required string Email { get; set; }
     public ICollection<Cheep> Cheeps { get; } = new List<Cheep>();
 }
