eea98502aff86cf2ce30a757073d1acd2ba5b0bb
Marcus Aandahl <marcus.aandahl@gmail.com>
2023-11-02 16:41:30 +0100

    Finish Create Cheep Implementation

--- src/Chirp.WebService/Controllers/CheepController.cs
+++ src/Chirp.WebService/Controllers/CheepController.cs
@@ -1,36 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Chirp.Core.Repositories;
-using Chirp.Infrastructure.Contexts;
-using Chirp.Infrastructure.Models;
-using Chirp.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Http;
+using Chirp.Core.Dto;
+using Chirp.WebService.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Chirp.WebService.Controllers
 {
     public class CheepController : Controller
     {
-        private readonly ICheepRepository _cheepRepository;
-        // // GET: Cheep
-        // public ActionResult Index()
-        // {
-        //     return View();
-        // }
-        //
-        // // GET: Cheep/Details/5
-        // public ActionResult Details(int id)
-        // {
-        //     return View();
-        // }
-        //
-        // // GET: Cheep/Create
-        // public ActionResult Create()
-        // {
-        //     return View();
-        // }
+        private readonly ICheepRepository _service;
+
+        public CheepController(ICheepRepository service)
+        {
+            _service = service;
+        }
 
         // POST: Cheep/Create
         [HttpPost]
@@ -40,62 +22,42 @@ namespace Chirp.WebService.Controllers
         {
             try
             {
-                // if (User.Identity != null && User.Identity.IsAuthenticated)
-                // {
-                //     _service.AddCheep(new AddCheepDto
-                //     {
-                //         AuthorEmail = User.GetUserEmail(),
-                //         AuthorName = User.GetUserFullName(),
-                //         Text = cheepText
-                //     });
-                // }
+                string? cheepText = collection["cheepText"];
+
+                if (cheepText == null)
+                {
+                    return BadRequest("Invalid input");
+                }
                 
-                // TODO: Add insert logic here
-                
-                return Accepted();
+                if (User.Identity != null && User.Identity.IsAuthenticated)
+                {
+                    _service.AddCheep(new AddCheepDto
+                    {
+                        AuthorEmail = User.GetUserEmail(),
+                        AuthorName = User.GetUserFullName(),
+                        Text = cheepText
+                    });
+                }
+                else
+                {
+                    return Unauthorized();
+                }
+
+                return Redirect(Request.GetPathUrl());
             }
             catch
             {
-                return BadRequest();
+                return BadRequest("Unknown Error Occurred");
             }
         }
 
-        // // GET: Cheep/Edit/5
-        // public ActionResult Edit(int id)
-        // {
-        //     return View();
-        // }
-        //
-        // // POST: Cheep/Edit/5
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public ActionResult Edit(int id, IFormCollection collection)
-        // {
-        //     try
-        //     {
-        //         // TODO: Add update logic here
-        //
-        //         return RedirectToAction(nameof(Index));
-        //     }
-        //     catch
-        //     {
-        //         return View();
-        //     }
-        // }
-        //
-        // // GET: Cheep/Delete/5
-        // public ActionResult Delete(int id)
-        // {
-        //     return View();
-        // }
-        //
         // POST: Cheep/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         [HttpPost]
         public IActionResult DeleteCheep(Guid id)
         {
-            _cheepRepository.DeleteCheep(id);
+            _service.DeleteCheep(id);
             return RedirectToAction(); // Replace "Index" with the name of your view that shows the list of cheeps.
         }
     }
