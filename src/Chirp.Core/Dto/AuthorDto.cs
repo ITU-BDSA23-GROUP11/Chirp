368910116a6290712edebed4b53a6616bf8e7411
Villads Grum-Schwensen <vilg@itu.dk>
2023-11-29 12:34:08 +0100

    Removed redundant authorDto method

--- src/Chirp.Core/Dto/AuthorDto.cs
+++ src/Chirp.Core/Dto/AuthorDto.cs
@@ -1,15 +1,6 @@
 namespace Chirp.Core.Dto;
-
-public class AddAuthorDto
-{
-    public required string Name { get; init; }
-    public required string Email { get; init; }
-    //public required IEnumerable<AuthorDto> FollowedBy { get; init; }
-}
-
 public class AuthorDto
 {
     public required string Name { get; init; }
-    public required string Email { get; init; }
-    //public required IEnumerable<AuthorDto> FollowedBy { get; init; }
+    public required string Email { get; init; } 
 }
