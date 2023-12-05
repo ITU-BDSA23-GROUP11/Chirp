618cb2cfe35588d4cc756a235d9ec7e352a76bca
Villads Grum-Schwensen <vilg@itu.dk>
2023-12-05 15:41:30 +0100

    Setup skeleton for improved test-methods with CustomWebApplication

---
+++ test/Chirp.WebService.Tests/E2ETests/PlaywrightTests.cs
@@ -0,0 +1,34 @@
+﻿using Testcontainers.SqlEdge;
+
+namespace Chirp.WebService.Tests.E2ETests;
+
+public class PlaywrightTests
+{
+    private readonly SqlEdgeContainer _sqlEdgeContainer = new SqlEdgeBuilder().Build();
+    
+    public Task InitializeAsync()
+    {
+        return _sqlEdgeContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _sqlEdgeContainer.DisposeAsync().AsTask();
+    }
+
+    private readonly string _serverAddress;
+
+    public PlaywrightTests(CustomWebApplicationFactory fixture)
+    {
+        _serverAddress = fixture.ServerAddress;
+    }
+
+    [Fact]
+    public async Task ClickAuthorNameRedirects()
+    {
+        //Arrange
+        
+    }
+
+
+}
