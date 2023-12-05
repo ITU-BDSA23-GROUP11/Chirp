fa18939892de34a4026b718f5014a7a51c34976c
Villads Grum-Schwensen <vilg@itu.dk>
2023-12-05 16:09:10 +0100

    Added an improved test and added unique ID to HTML for easy XPath

--- test/Chirp.WebService.Tests/E2ETests/PlaywrightTests.cs
+++ test/Chirp.WebService.Tests/E2ETests/PlaywrightTests.cs
@@ -1,8 +1,11 @@
-﻿using Testcontainers.SqlEdge;
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using Renci.SshNet;
+using Testcontainers.SqlEdge;
 
 namespace Chirp.WebService.Tests.E2ETests;
 
-public class PlaywrightTests
+public class PlaywrightTests : PageTest, IClassFixture<CustomWebApplicationFactory>
 {
     private readonly SqlEdgeContainer _sqlEdgeContainer = new SqlEdgeBuilder().Build();
     
@@ -27,7 +30,27 @@ public class PlaywrightTests
     public async Task ClickAuthorNameRedirects()
     {
         //Arrange
+        using var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
         
+            //XPath for author name
+            var authorXPath = "//*[@id=\"messagelist\"]/li[1]/p[1]/strong/a";
+        
+        await page.GotoAsync(_serverAddress);
+    
+        var authorButton = await page.QuerySelectorAsync(authorXPath);
+
+        var authorName = await authorButton.InnerTextAsync();
+            
+        //Act
+        await authorButton.ClickAsync();//Simulate click
+        
+        var headerElement = await page.WaitForSelectorAsync("//*[@id='userTimeline']");
+
+        var headerElementValue = await headerElement.InnerTextAsync();
+
+        Assert.Contains(authorName, headerElementValue);
     }
 
 
