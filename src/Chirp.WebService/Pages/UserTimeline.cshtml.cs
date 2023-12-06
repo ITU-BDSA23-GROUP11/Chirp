4eefc9182dd12e7ddd15b7302e2dc215f494488d
Andreas <anbc@itu.dk>
2023-12-06 19:10:23 +0100

    Shows amount of likes on each cheep

--- src/Chirp.WebService/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.WebService/Pages/UserTimeline.cshtml.cs
@@ -77,4 +77,9 @@ public class UserTimelineModel : PageModel
         return _likeRepository.IsLiked(authorId, cheepId);   
     
     }
+    
+    public int GetLikeCount(Guid cheepId)
+    {
+        return _likeRepository.LikeCount(cheepId);
+    }
 }
