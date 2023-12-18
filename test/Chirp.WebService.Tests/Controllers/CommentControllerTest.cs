bd1146e8327f42f7b1b051b54700c1cec54f26c8
Andreas <anbc@itu.dk>
2023-12-18 18:33:46 +0100

    Additional controller tests

--- test/Chirp.WebService.Tests/Controllers/CommentControllerTest.cs
+++ test/Chirp.WebService.Tests/Controllers/CommentControllerTest.cs
@@ -3,6 +3,7 @@ using Chirp.Core.Extensions;
 using Chirp.Infrastructure.Models;
 using Chirp.Tests.Core;
 using Chirp.WebService.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
 using Moq;
 
@@ -41,8 +42,73 @@ public class CommentControllerTest
     [Fact]
     public void TestAddComment()
     {
+        //Arrange
         Author author = _mockChirpRepositories.TestAuthors.First();
         Cheep cheep = _mockChirpRepositories.TestCheeps.First();
-      
+        string cheepId = cheep.CheepId.ToString();
+        string authorId = author.AuthorId.ToString();
+        string commentText = new Faker().Random.Words();
+        
+        IFormCollection collection = new FormCollection(
+            new Dictionary<string, StringValues>
+            {
+                { "CommentText", commentText },
+                { "CheepId", cheepId },
+                { "AuthorId", authorId }
+            }
+        );
+
+        // Act
+        var result = _commentController.Add(collection);
+
+        // Assert
+        Assert.IsType<RedirectResult>(result);
+        var redirectResult = result as RedirectResult;
+        Assert.NotNull(redirectResult);
+        Assert.False(string.IsNullOrEmpty(redirectResult.Url));
+    }
+
+    [Fact]
+    public void TestDeleteComment()
+    {
+        //Arrange
+        Comment comment = _mockChirpRepositories.TestComment.First();
+        string commentId = comment.CommentId.ToString();
+
+        IFormCollection collection = new FormCollection(
+            new Dictionary<string, StringValues>
+            {
+                { "CommentId", commentId }
+            }
+        );
+        
+        // Act
+        var result = _commentController.Delete(collection);
+
+        // Assert
+        Assert.IsType<RedirectResult>(result);
+        var redirectResult = result as RedirectResult;
+        Assert.NotNull(redirectResult);
+        Assert.False(string.IsNullOrEmpty(redirectResult.Url));
+    }
+
+   [Fact] 
+    public void TestDeleteCommentFail()
+    {
+        //Arrange
+        string fakeCommentId = "not_an_id";
+
+        IFormCollection collection = new FormCollection(
+            new Dictionary<string, StringValues>
+            {
+                { "CommentId", fakeCommentId }
+            }
+        );
+        
+        //Act
+        IActionResult actionResult = _commentController.Delete(collection);
+        
+        Assert.False(actionResult is RedirectResult);
+
     }
 }
