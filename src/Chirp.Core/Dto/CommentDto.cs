f34008c672bc1762d79f6010b089928462475696
Villads Grum-Schwensen <vilg@itu.dk>
2023-12-18 11:38:04 +0100

    Added a DTO for comments -> needs to include CheepId to ensure deletion ability

---
+++ src/Chirp.Core/Dto/CommentDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Chirp.Core.Dto;
+
+public class AddCommentDto
+{
+    public required Guid AuthorId { get; init; }
+    public required Guid CheepId { get; init; }
+    public required string Text { get; init; }
+}
+
+public class CommentDto
+{
+    public required Guid CommentId { get; init; }
+    public required Guid AuthorId { get; init; }
+    
+    public required Guid CheepId { get; init; }
+    public required string? AuthorName { get; init; }
+    public required string AuthorUsername { get; init; }
+    public required string AuthorAvatarUrl { get; init; }
+    public required string Text { get; init; }
+    public required DateTime Timestamp { get; init; }
+}
