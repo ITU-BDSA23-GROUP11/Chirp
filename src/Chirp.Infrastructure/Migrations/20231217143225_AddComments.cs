1e0fb116ec3ff75a3ec820e513e98513219b51ce
Andreas <anbc@itu.dk>
2023-12-18 11:04:11 +0100

    Report start

---
+++ src/Chirp.Infrastructure/Migrations/20231217143225_AddComments.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Authors_AuthorId",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Cheeps_CheepId",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameColumn(
+                name: "CheepId",
+                table: "Comments",
+                newName: "cheepId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "cheepId",
+                table: "Comments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AuthorId",
+                table: "Comments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CommentId",
+                table: "Comments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Comments",
+                type: "nvarchar(160)",
+                maxLength: 160,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Timestamp",
+                table: "Comments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "CommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_cheepId",
+                table: "Comments",
+                column: "cheepId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Cheeps_cheepId",
+                table: "Comments",
+                column: "cheepId",
+                principalTable: "Cheeps",
+                principalColumn: "CheepId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Cheeps_cheepId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_cheepId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CommentId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameColumn(
+                name: "cheepId",
+                table: "Comment",
+                newName: "CheepId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CheepId",
+                table: "Comment",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AuthorId",
+                table: "Comment",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Authors_AuthorId",
+                table: "Comment",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Cheeps_CheepId",
+                table: "Comment",
+                column: "CheepId",
+                principalTable: "Cheeps",
+                principalColumn: "CheepId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
